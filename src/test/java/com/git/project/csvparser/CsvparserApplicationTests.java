d416db0ef97d2d8be22b72aa39232a4fe03ec4df
yermukhanJJ <ermuhannurkali@gmail.com>
2023-02-08 23:17:23 +0600

    test

---
+++ src/test/java/com/git/project/csvparser/CsvparserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.project.csvparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvparserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
