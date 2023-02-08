602bb468d3c9e5f0b8893d6a17f87e14c19eb333
yermukhanJJ <ermuhannurkali@gmail.com>
2023-02-08 23:17:17 +0600

    application main class

---
+++ src/main/java/com/git/project/csvparser/CsvparserApplication.java
@@ -0,0 +1,13 @@
+package com.git.project.csvparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvparserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvparserApplication.class, args);
+	}
+
+}
